3ac294ff3bdf26f695059e185ab03fe628cf5e9b
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2025-01-01 02:58:49 -0600

    feat: Implemented messages for update, create and delete actions

--- dish_management/views.py
+++ dish_management/views.py
@@ -7,25 +7,29 @@ from django.views.generic import (
 )
 
 from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 
 from dish_management.forms import CreateDishForm, UpdateDishForm
 from dish_management.models import Dish
 
 
 # Create your views here.
-class DishCreateView(CreateView):
+class DishCreateView(SuccessMessageMixin, CreateView):
     model = Dish
     form_class = CreateDishForm
     template_name = "dish_management/create.html"
     success_url = reverse_lazy("dish-list")
+    success_message = "<b>%(name)s </b> fue creado correctamente"
 
 
-class DishUpdateView(UpdateView):
+class DishUpdateView(SuccessMessageMixin, UpdateView):
     model = Dish
     form_class = UpdateDishForm
     template_name = "dish_management/update.html"
     context_object_name = "dish"
     success_url = reverse_lazy("dish-list")
+    success_message = "<b>%(name)s </b> fue actualizado correctamente"
 
 
 class DishDetailView(DetailView):
@@ -44,4 +48,5 @@ class DishListView(ListView):
 def delete_dish(request, pk):
     dish = get_object_or_404(Dish, pk=pk)
     dish.delete()
+    messages.success(request, f"<b>{dish.name}</b> fue eliminado correctamente")
     return redirect("dish-list")
