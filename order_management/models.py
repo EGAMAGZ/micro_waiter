4fe173144624766ce8d68ca20bec1a01951ba593
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2025-01-06 23:08:45 -0600

    feat(order_managemnet): Created model for order and its migrations

--- order_management/models.py
+++ order_management/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+from dish_management.models import Dish
+
+
 # Create your models here.
+class Order(models.Model):
+    total = models.IntegerField(
+        verbose_name="Pago total",
+    )
+
+    dishes = models.ManyToManyField(
+        Dish,
+        verbose_name="Platillos pedidos",
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Creado",
+        editable=False,
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name="Editado",
+        editable=False,
+    )
